0edcdaa44f9e9b28ab832834584df4ea0f26a243
Hernan Di Tano <hditano@gmail.com>
2021-09-05 02:19:42 -0300

    added basic logic for ghosts movement

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const grid = document.querySelector('.grid');
+const scoreLabel = document.querySelector('.score');
 let squares = [];
+let ghosts = [];
 let width = 28;
+let score = 0;
 
 const layout = [
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -44,6 +47,8 @@ function createGrid() {
             squares[i].classList.add('wall');
         } else if (layout[i] === 2) {
             squares[i].classList.add('ghost-lair');
+        } else if (layout[i] === 0) {
+            squares[i].classList.add('pac-dots');
         }
 
     }
@@ -59,20 +64,20 @@ function control(e) {
     squares[currentPacmanIndex].classList.remove('pacman');
     switch (e.key) {
         case 'ArrowLeft':
-            console.log('Arrow Left');
             if (!squares[currentPacmanIndex - 1].classList.contains('ghost-lair') && !squares[currentPacmanIndex - 1].classList.contains('wall') && currentPacmanIndex % width !== 0) currentPacmanIndex -= 1;
+            pacDotEaten()
             break;
         case 'ArrowRight':
-            console.log('Arrow Right');
             if (!squares[currentPacmanIndex + 1].classList.contains('ghost-lair') && !squares[currentPacmanIndex + 1].classList.contains('wall') && currentPacmanIndex % width < width - 1) currentPacmanIndex += 1;
+            pacDotEaten()
             break;
         case 'ArrowDown':
-            console.log('Arrow Down');
             if (!squares[currentPacmanIndex + width].classList.contains('ghost-lair') && !squares[currentPacmanIndex + width].classList.contains('wall') && currentPacmanIndex + width < width * width) currentPacmanIndex = currentPacmanIndex += width;
+            pacDotEaten()
             break;
         case 'ArrowUp':
             if (!squares[currentPacmanIndex - width].classList.contains('ghost-lair') && !squares[currentPacmanIndex - width].classList.contains('wall') && currentPacmanIndex - width >= 0) currentPacmanIndex -= width;
-            console.log('Arrow Up');
+            pacDotEaten()
             break;
     }
     squares[currentPacmanIndex].classList.add('pacman');
@@ -80,3 +85,51 @@ function control(e) {
 
 
 document.addEventListener('keyup', control);
+
+function pacDotEaten() {
+    if (squares[currentPacmanIndex].classList.contains('pac-dots')) {
+        score++;
+        squares[currentPacmanIndex].classList.remove('pac-dots');
+        scoreLabel.textContent = score;
+    }
+}
+
+class Ghosts {
+    constructor(ghostName, index, speed) {
+        this.ghostName = ghostName;
+        this.index = index;
+        this.speed = speed;
+        this.currentIndex = index;
+        this.timerID = NaN;
+    }
+}
+
+const createGhosts = [
+    new Ghosts('Toti', 348, 270),
+    new Ghosts('Tata', 376, 270),
+    new Ghosts('Toto', 351, 270),
+]
+
+createGhosts.forEach(ghost => squares[ghost.index].classList.add('ghosts'));
+
+
+// move ghosts 
+
+createGhosts.forEach(ghost => moveGhost(ghost));
+
+function moveGhost(ghost) {
+    console.log('moved ghost');
+    const directions = [-1, +2, +width, -width];
+
+    let direction = directions[Math.floor(Math.random() * directions.length)];
+
+    console.log(direction);
+
+    ghost.timerID = setInterval(function() {
+
+        squares[ghost.currentIndex].classList.remove('ghosts');
+        ghost.currentIndex += direction;
+        squares[ghost.currentIndex].classList.add('ghosts');
+
+    }, ghost.speed);
+}
